bf1c8ab1fc58efe11aa096b12798bca065181331
@oana.novac <novac.oana@yahoo.com>
2025-07-24 15:46:58 +0300

    Remove what I don't have time to finish

--- src/main/java/org/example/ss_2022_c11_e1/config/SecurityConfig.java
+++ src/main/java/org/example/ss_2022_c11_e1/config/SecurityConfig.java
@@ -1,16 +1,8 @@
 package org.example.ss_2022_c11_e1.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    @
-    public SecurityFilterChain asSecurityFilterChaine(HttpSecurity http) {
-
-    }
 }
